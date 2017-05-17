22600e42c77a8346f00e9434eff4b663e0560521
Clitanio Guimaraes <Clitanio@hotmail.com>
2017-05-17 10:44:15 -0300

    Utilizando o Git através do Netbeans

--- src/exemplo_git_nebeans/Exemplo_Git_Nebeans.java
+++ src/exemplo_git_nebeans/Exemplo_Git_Nebeans.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exemplo_git_nebeans;
 
-/**
- *
- * @author clitanio
- */
+import javax.swing.JOptionPane;
 public class Exemplo_Git_Nebeans {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Utilizando o Git através do Netbeans");
+        JOptionPane.showMessageDialog(null,"Utilizando o Git através do Netbeans");
+        //Alteração via netbeans
+        //Alteração via web
     }
     
 }
